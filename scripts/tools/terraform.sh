9cefaae45e192d0a5d68736ca5d175192123a640
Clément <clement.deseine@gmail.com>
2019-12-22 22:09:14 +0100

    add script to download terraform

--- scripts/tools/terraform.sh
+++ scripts/tools/terraform.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+TERRAFORM_DIR="/opt/terraform/$1"
+
+if [ ! -d "$TERRAFORM_DIR" ]; then
+  mkdir -p "$TERRAFORM_DIR"
+  wget https://releases.hashicorp.com/terraform/$1/terraform_$1_linux_amd64.zip -O "$TERRAFORM_DIR/terraform_$1_linux_amd64.zip"
+  unzip "$TERRAFORM_DIR/terraform_$1_linux_amd64.zip" -d "$TERRAFORM_DIR"
+  cp "$TERRAFORM_DIR/terraform" "/usr/local/bin/terraform$1"
+  chmod +x "/usr/local/bin/terraform$1"
+fi
+
+if [ "$2" = 1 ]; then
+  cp "$TERRAFORM_DIR/terraform" "/usr/local/bin/terraform"
+fi
