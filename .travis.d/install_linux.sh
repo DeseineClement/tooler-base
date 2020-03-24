1ad5f5f6fa9b189a94dbdcdda2b48088bb68ed94
Clément <clement.deseine@gmail.com>
2020-03-24 19:05:04 +0100

    added Ansible in Travis CI

--- .travis.d/install_linux.sh
+++ .travis.d/install_linux.sh
@@ -3,7 +3,9 @@
 wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y linux-headers-`uname -r` virtualbox
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y linux-headers-`uname -r` virtualbox ansible
 wget https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_linux_amd64.zip
 unzip packer_1.5.4_linux_amd64.zip
 sudo mv packer /usr/local/bin
