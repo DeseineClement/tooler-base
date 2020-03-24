4be33282fcbbe380bdf987ba799d59d56dd72501
Clément <clement.deseine@gmail.com>
2020-03-24 18:34:54 +0100

    remove sudo from osx

--- .travis.d/install_linux.sh
+++ .travis.d/install_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | apt-key add -
-apt-get update
-apt-get install linux-headers-`uname -r` virtualbox
+wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install linux-headers-`uname -r` virtualbox
