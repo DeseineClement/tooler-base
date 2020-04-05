a54ba8e89ef3de0c672d5af6eba234c50ed89c34
Clément Deseine <cdeseine@wedigital.garden>
2020-04-05 12:07:59 +0200

    fixed linux install

--- .travis.d/install_linux.sh
+++ .travis.d/install_linux.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+PACKER_VERSION=1.5.5
+VBOX_VERSION=6.1
+
+sudo apt upgrade -y
+sudo apt install -y software-properties-common
+sudo apt-add-repository --yes ppa:ansible/ansible
 wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y linux-headers-`uname -r` virtualbox ansible
-wget https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_linux_amd64.zip
-unzip packer_1.5.4_linux_amd64.zip
+sudo apt update
+
+sudo apt install -y linux-headers-"$(uname -r)" virtualbox-"${VBOX_VERSION}" ansible
+wget https://releases.hashicorp.com/packer/"${PACKER_VERSION}"/packer_"${PACKER_VERSION}"_linux_amd64.zip
+unzip packer_"${PACKER_VERSION}"_linux_amd64.zip
 sudo mv packer /usr/local/bin
